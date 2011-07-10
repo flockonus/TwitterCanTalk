056bd5e8e168e5772b8f191a77fc767e3290a75f
FabianoPS <fabianosoriani@gmail.com>
2011-07-10 19:54:23 -0300

    Minnor filter improvment

--- src/js/TwitterReader.js
+++ src/js/TwitterReader.js
@@ -69,7 +69,10 @@ function TwitterCanTalk(){
   
   function treat_and_queue(txt){
     
-    txt = ( escape( remove_links(txt).substring(0,100) ) )
+    txt = ( escape( remove_links(txt).replace( /(\#|\@)/g, '' ) ) )
+    
+    // google TTS limit
+    txt = txt.substring(0,100)
     
     instance.queue.push( "http://translate.google.com/translate_tts?ie=UTF-8&tl=pt&q=" + txt )
     console.log('Added 1!')
